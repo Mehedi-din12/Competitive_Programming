44e2cf77e25ea5998ce4399633e486df45cd11ed
mehedidin <mehedi.hasan36@northsouth.edu>
2022-09-13 13:04:33 +0600

    first commit

---
+++ contest.cpp
@@ -0,0 +1,48 @@
+//In the name of ALLAH
+
+#include<bits/stdc++.h>
+using namespace std;
+//================================================================================================
+#define fast_io   ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define nl "\n"
+#define pi acos(-1.00)//2.0*acos(0.0)
+//================================================================================================
+typedef long long int lli;
+//================================================================================================
+const lli INF = 1e18;
+const lli N_INF = -1e18;
+const int N=1e6;//10^3=1000=1e3
+const double EPS = (double) 1e-9;
+//================================================================================================
+
+
+
+//================================================================================================
+
+void solve(){
+
+  cout<<"Hello Vs Code"<<nl;
+    
+}
+
+//================================================================================================
+void tcType1(){solve();}
+void tcType2(){int t;cin>>t;cin.ignore(); while (t--){solve();}}
+void tcType3(){int t;cin>>t;for(int i=1;i<=t;++i){cout<<"Case "<<i<<": ";solve();}}
+void init(){
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);freopen("error.txt","w",stderr);
+#endif  
+}
+//================================================================================================
+
+int main(){
+  init();
+  fast_io;
+
+  tcType1();
+  //tcType2();
+  //tcType3();
+
+  return 0;
+}
